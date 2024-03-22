a2c586ca97c4f04204d2665a6444794017de0035
sixzjkk <jenifergmporfirio@gmail.com>
2024-03-22 16:11:01 -0400

    atividade 3

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,10 +3,24 @@ import { useState } from "react";
 export default function Main() {
     const [nome, setNome] = useState("");
     const [telefone, setTel] = useState(0);
+    const [cpf, setCpf] = useState(0);
+    const [listaContatos, setContatos] = useState([]);
+
+    const registrar = (event) => {
+        event.preventDefault();
+        setContatos([...listaContatos,{
+            nomeSalvo: nome,
+            telefoneSalvo: telefone,
+            cpfSalvo: cpf,
+        }
+    ])
+        alert("Cadastro salvo");
+    };
+    console.table(listaContatos);
 
     return(
         <main>
-            <form>
+            <form action="" onSubmit={registrar}>
                 <label htmlFor="nome">Nome:</label>
                 <input
                     type= "text"
@@ -19,9 +33,16 @@ export default function Main() {
                     name= "" id= ""
                     onChange={(event)=> setTel(event.target.value)}
                 />
+                <label htmlFor="cpf">CPF:</label>
+                <input
+                    type= "tel"
+                    name= "" id= ""
+                    onChange={(event)=> setCpf(event.target.value)}
+                />
                 {nome}
                 {telefone}
-                <button>Enviar</button>
+                {cpf}
+                <button>Salvar</button>
             </form>
         </main>
     )
